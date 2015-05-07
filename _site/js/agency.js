08e9cfb6b626ddace0873c8a5b97771195b16118
Jan Kus <jan@railslove.com>
2015-05-07 17:01:33 +0200

    added video

--- _site/js/agency.js
+++ _site/js/agency.js
@@ -46,7 +46,7 @@ $(function() {
     // Grab the current date
     var currentDate = new Date();
     // Set some date in the future. In this case, it's always Jan 1
-    var futureDate  = new Date("March 12, 2015 11:13:00");
+    var futureDate  = new Date("March 12, 2016 11:13:00");
     // Calculate the difference in seconds between the future and current date
     var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
     // Instantiate a coutdown FlipClock
